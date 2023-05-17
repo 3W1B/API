107410039dab6334495f71c683f72bc7b387e10f
marpond <sonkoly.marci02@gmail.com>
2023-05-17 11:05:02 +0200

    scaffolded the database

---
+++ RadonAPI/Entities/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RadonAPI.Entities;
+
+public partial class Log
+{
+    public int Id { get; set; }
+
+    public int? RadonLoggerId { get; set; }
+
+    public DateTime? Timestamp { get; set; }
+
+    public virtual ICollection<LogInside> LogInsides { get; set; } = new List<LogInside>();
+
+    public virtual ICollection<LogOutside> LogOutsides { get; set; } = new List<LogOutside>();
+
+    public virtual RadonLogger? RadonLogger { get; set; }
+}
