f7ba4dd8910821c4697a15033c9e9177dda79660
Patrick Schemel <Patrick@schemel.dk>
2023-05-19 09:59:29 +0200

    Added K6Tests

---
+++ Tests/K6SoakTest.js
@@ -0,0 +1,35 @@
+import http from 'k6/http'
+import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+    threshold: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<200']
+    },
+    duration: '5s',
+    vus: 10,
+    stages:
+        [
+            { duration: '20s', target: 400 },
+            { duration: '10s', target: 400 },
+            { duration: '20s', target: 0 },
+        ]
+}
+
+export default function () {
+    const data = { id: 1 };
+    let res = http.post(`http://${__ENV.HOSTING}/log/readall`, JSON.stringify(data),
+        {
+            headers: { 'Content-Type': 'application/json' }
+        }
+    );
+    const checkSuccess = check(res, { 'Task Read Success': (r) => r.status === 200 });
+    if (!checkSuccess) fail('Task Read Failed');
+}
+
+export function handleSummary(data) {
+    return {
+        "K6Reports/SoakTestSummary.html": htmlReport(data),
+    };
+}
