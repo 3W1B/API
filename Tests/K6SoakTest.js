0f9a339711362031a27818cdfea8cde607cc4b30
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 14:13:08 +0200

    Update K6SoakTest.js

--- Tests/K6SoakTest.js
+++ Tests/K6SoakTest.js
@@ -3,19 +3,18 @@ import { check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    threshold: {
-        http_req_failed: ['rate<0.01'],
-        http_req_duration: ['p(95)<200']
-    },
-    duration: '5s',
-    vus: 10,
-    stages:
-        [
-            { duration: '20s', target: 400 },
-            { duration: '10s', target: 400 },
-            { duration: '20s', target: 0 },
-        ]
-}
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<200'],
+  },
+};
+
+export const stages = [
+  { duration: '20s', target: 400 },
+  { duration: '20s', target: 400 },
+  { duration: '20s', target: 0 },
+
+];
 
 export default function () {
     const data = { id: 1 };
