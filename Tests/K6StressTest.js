1c1d3bf3b0555a05b070325cfffa4ba148fba304
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 15:23:35 +0200

    Update K6StressTest.js

--- Tests/K6StressTest.js
+++ Tests/K6StressTest.js
@@ -24,6 +24,7 @@ export default function () {
     headers: { 'Content-Type': 'application/json' },
   });
   const checkSuccess = check(res, { 'Logger Read Success': (r) => r.status === 200 });
+  if (!checkSuccess) fail('Logger Read Failed');
 }
 
 export function handleSummary(data) {
