5cec1e6f678375817babdb5d61346ce3ab56829b
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 14:07:26 +0200

    Update K6StressTest.js

--- Tests/K6StressTest.js
+++ Tests/K6StressTest.js
@@ -3,18 +3,21 @@ import { check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-  duration: '5s',
-  vus: 10,
-  stages: [
-    { duration: '20s', target: 15 },
-    { duration: '20s', target: 15 },
-    { duration: '20s', target: 20 },
-    { duration: '20s', target: 20 },
-    { duration: '20s', target: 50 },
-    { duration: '20s', target: 50 },
-    { duration: '20s', target: 0 },
-  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<200'],
+  },
 };
+
+export const stages = [
+  { duration: '20s', target: 15 },
+  { duration: '20s', target: 15 },
+  { duration: '20s', target: 20 },
+  { duration: '20s', target: 20 },
+  { duration: '20s', target: 50 },
+  { duration: '20s', target: 50 },
+  { duration: '20s', target: 0 },
+];
 
 export default function () {
   const data = { id: 1 };
