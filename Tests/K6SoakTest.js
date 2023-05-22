d01defbf9550368ca879d872f9ff9db59e5c9e7d
Marpond <89220481+Marpond@users.noreply.github.com>
2023-05-22 15:23:19 +0200

    Update K6SoakTest.js

--- Tests/K6SoakTest.js
+++ Tests/K6SoakTest.js
@@ -23,6 +23,7 @@ export default function () {
         }
     );
     const checkSuccess = check(res, { 'Logger Read Success': (r) => r.status === 200 });
+    if (!checkSuccess) fail('Logger Read Failed');
 }
 
 export function handleSummary(data) {
