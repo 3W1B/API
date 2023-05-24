56e5826cd5da517cb4f6b56a0652964e41ef4e27
Marpond <sonkoly.marci02@gmail.com>
2023-05-24 11:44:21 +0200

    added LocationController.cs, added password to LogController.Create

---
+++ RadonAPI/Requests/LocationRequest.cs
@@ -0,0 +1,49 @@
+﻿using RadonAPI.Entities;
+using RadonAPI.Models;
+using RadonAPI.Utils;
+
+namespace RadonAPI.Requests;
+
+public abstract class LocationRequest
+{
+    public static dynamic? Update(dynamic body, out CustomResponse? customResponse)
+    {
+        if (ParameterHandler.IsNull(body.loggerId))
+        {
+            customResponse = new CustomResponse("error", "Logger id is null");
+            return null;
+        }
+        
+        if (ParameterHandler.IsNull(body.loggerPassword))
+        {
+            customResponse = new CustomResponse("error", "Logger password is null");
+            return null;
+        }
+        
+        if (ParameterHandler.IsNull(body.latitude))
+        {
+            customResponse = new CustomResponse("error", "Latitude is null");
+            return null;
+        }
+        
+        if (ParameterHandler.IsNull(body.longitude))
+        {
+            customResponse = new CustomResponse("error", "Longitude is null");
+            return null;
+        }
+
+        Location location = new()
+        {
+            LoggerId = body.loggerId.ToString(),
+            Latitude = body.latitude,
+            Longitude = body.longitude
+        }; 
+        
+        customResponse = null;
+        return new
+        {
+            Location = location,
+            LoggerPassword = body.loggerPassword.ToString()
+        };
+    }
+}
---
+++ Tests/LocationTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using RadonAPI.Controllers;
+
+namespace Tests;
+
+public class LocationTests
+{
+    [Test]
+    public async Task Update()
+    {
+        var body = new Dictionary<string, dynamic>
+        {
+            { "loggerId", "testid" },
+            { "loggerPassword", "testpassword"},
+            { "latitude", RandomNumberGenerator.GetInt32(0, 100) + RandomNumberGenerator.GetInt32(0, 100) / 100.0 },
+            { "longitude", RandomNumberGenerator.GetInt32(0, 100) + RandomNumberGenerator.GetInt32(0, 100) / 100.0 }
+        };
+
+        LocationController locationController = new();
+
+        await TestHandler.Run(body, Expect.Success, locationController, locationController.Update);
+    }
+}
